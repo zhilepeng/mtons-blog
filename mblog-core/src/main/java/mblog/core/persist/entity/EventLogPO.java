0c97799bac064db916c487538fc12e9c53b04ef3
langhsu <langhsu@Langhsu>
2015-04-14 22:25:17 +0800

    add entity: event log

---
+++ mblog-core/src/main/java/mblog/core/persist/entity/EventLogPO.java
@@ -0,0 +1,90 @@
+/**
+ * 
+ */
+package mblog.core.persist.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ * 
+ * 事件日志
+ * 
+ * @author langhsu
+ *
+ */
+@Entity
+@Table(name = "tb_event_log")
+public class EventLogPO {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "user_id")
+	private long userId;
+	private int type;
+	
+	@Column(name = "target_id")
+	private long targetId;
+	private String ip;
+	
+	@Temporal(TemporalType.DATE)
+	private Date created;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public long getTargetId() {
+		return targetId;
+	}
+
+	public void setTargetId(long targetId) {
+		this.targetId = targetId;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+	
+}
