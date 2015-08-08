e32340412b0b8571c13a01f985f9af3fc935d03c
langhsu <langhsu@gmail.com>
2015-08-08 21:40:04 +0800

    解决用户禁用时登录提示信息不对的问题

--- mblog-web/src/main/java/mblog/web/controller/LoginController.java
+++ mblog-web/src/main/java/mblog/web/controller/LoginController.java
@@ -5,20 +5,17 @@
  *******************************************************************************/
 package mblog.web.controller;
 
+import mblog.web.controller.desk.Views;
 import org.apache.commons.lang.StringUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.LockedAccountException;
 import org.apache.shiro.authc.UnknownAccountException;
-import org.apache.shiro.authc.UsernamePasswordToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import mblog.web.controller.desk.Views;
-import mtons.modules.utils.MD5Helper;
 
 /**
  * 
@@ -59,6 +56,8 @@ public class LoginController extends BaseController {
             } else {
             	model.put("message", "用户认证失败");
             }
+        } catch (IllegalStateException e) {
+            model.put("message", e.getMessage());
         }
         return ret;
 	}
