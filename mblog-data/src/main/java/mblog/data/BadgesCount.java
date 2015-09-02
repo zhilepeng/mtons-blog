7da6524d9c274b04bdee93fdf0356b2f584bb20f
langhsu <langhsu@gmail.com>
2015-09-02 21:16:30 +0800

    添加序列化

--- mblog-data/src/main/java/mblog/data/BadgesCount.java
+++ mblog-data/src/main/java/mblog/data/BadgesCount.java
@@ -1,10 +1,14 @@
 package mblog.data;
 
+import java.io.Serializable;
+
 /**
  * @author langhsu on 2015/8/31.
  */
-public class BadgesCount {
-    private int notifies; // 通知数量
+public class BadgesCount implements Serializable {
+	private static final long serialVersionUID = 8276459939240769498L;
+
+	private int notifies; // 通知数量
     private int messages; // 私信数量
 
     public int getNotifies() {
