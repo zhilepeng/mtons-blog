fb8ee73c03b00bb0697314353663dfdbfdc9713a
Beldon <1047325205@qq.com>
2015-10-31 16:26:55 +0800

    尝试使用钩子、插件功能

--- mblog-web/src/main/java/mblog/web/interceptor/BaseInterceptor.java
+++ mblog-web/src/main/java/mblog/web/interceptor/BaseInterceptor.java
@@ -12,6 +12,8 @@ package mblog.web.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import mblog.core.hook.interceptor.InterceptorHookManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -24,14 +26,31 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
  */
 public class BaseInterceptor extends HandlerInterceptorAdapter {
 
+	@Autowired
+	private InterceptorHookManager interceptorHookManager;
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		interceptorHookManager.preHandle(request, response, handler);
 		return true;
 	}
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
 		request.setAttribute("base", request.getContextPath());
+		interceptorHookManager.postHandle(request,response,handler,modelAndView);
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+		super.afterCompletion(request, response, handler, ex);
+		interceptorHookManager.afterCompletion(request, response, handler, ex);
+	}
+
+	@Override
+	public void afterConcurrentHandlingStarted(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		super.afterConcurrentHandlingStarted(request, response, handler);
+		interceptorHookManager.afterConcurrentHandlingStarted(request, response, handler);
 	}
 
 }
